3318c0a0db4f88c7040c3fe2acd1e4a50827266c
Ghost <s.lunev88@gmail.com>
2019-07-29 23:34:27 +0300

    feat: change developers pic style

--- src/components/layer.js
+++ src/components/layer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './header'
 import { Layout, Row, Col } from 'antd';
-import user from '../images/user.jpg';
 import github from '../images/github.png';
 
 import '../styles/layout.css'
@@ -14,12 +13,12 @@ const Layer = ({ children, path }) => {
     <Header path={path} />
     <main>{children}</main>
     <Footer style={{ textAlign: 'center' }}>
-      <div>Разработчики</div>
+      <div className="delepopers-header">Наша команда разработчиков</div>
       <Row className="developers">
         <Col span={4}>
           <div className="developer-card">
             <p>Евгений</p>
-            <img alt='Евгений' src={user} />
+            <img alt='Евгений' src="https://avatars1.githubusercontent.com/u/47753023"></img>
             <div className="github">
               <a href='https://github.com/eugenio7779' rel="noopener noreferrer" target='_blank'>
                 <img alt='eugenio7779' src={github}></img>
@@ -30,7 +29,7 @@ const Layer = ({ children, path }) => {
         <Col span={4}>
           <div className="developer-card">
             <p>Георг</p>
-            <img alt='Георг' src={user} />
+            <img alt='Георг' src="https://avatars2.githubusercontent.com/u/34744343"></img>
             <div className="github">
               <a href='https://github.com/georgpandeh' rel="noopener noreferrer" target='_blank'>
                 <img alt='georgpandeh' src={github}></img>
@@ -41,7 +40,7 @@ const Layer = ({ children, path }) => {
         <Col span={4}>
           <div className="developer-card">
             <p>Сергей Лунёв</p>
-            <img alt='Сергей Лунёв' src={user} />
+            <img alt='Сергей Лунёв' src="https://avatars2.githubusercontent.com/u/4992801"></img>
             <div className="github">
               <a href='https://github.com/venul' rel="noopener noreferrer" target='_blank'>
                 <img alt='venul' src={github}></img>
@@ -52,7 +51,7 @@ const Layer = ({ children, path }) => {
         <Col span={4}>
           <div className="developer-card">
             <p>Виталий</p>
-            <img alt='Сергей Лунёв' src={user} />
+            <img alt='Виталий' src="https://avatars3.githubusercontent.com/u/44432156"></img>
             <div className="github">
               <a href='https://github.com/moiseenokv' rel="noopener noreferrer" target='_blank'>
                 <img alt='moiseenokv' src={github}></img>
@@ -63,7 +62,7 @@ const Layer = ({ children, path }) => {
         <Col span={4}>
           <div className="developer-card">
             <p>Анна</p>
-            <img alt='Анна' src={user} />
+            <img alt='Анна' src="https://avatars1.githubusercontent.com/u/47710110"></img>
             <div className="github">
               <a href='https://github.com/annakiniova' rel="noopener noreferrer" target='_blank'>
                 <img alt='annakiniova' src={github}></img>
@@ -74,7 +73,7 @@ const Layer = ({ children, path }) => {
         <Col span={4}>
           <div className="developer-card">
             <p>Евгения</p>
-            <img alt='Евгения' src={user} />
+            <img alt='Евгения' src="https://avatars1.githubusercontent.com/u/9293095"></img>
             <div className="github">
               <a href='https://github.com/yevgeniyaa' rel="noopener noreferrer" target='_blank'>
                 <img alt='yevgeniyaa' src={github}></img>
