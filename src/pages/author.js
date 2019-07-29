50815984fcfe1ad081ee6c842f1409f6e4ca36f3
Anna Kiniova <anna.kiniova@gmail.com>
2019-07-29 19:49:31 +0300

    feat: add author page component

---
+++ src/pages/author.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MyMap from '../components/mapWidget';
+
+const AuthorPage = () => {
+    return (
+        <div>  
+            <MyMap />
+        </div>
+        
+    )
+}
+
+export default AuthorPage;
