48ad69cf72da44123df0cda26e45fcf593030b2f
Anna Kiniova <anna.kiniova@gmail.com>
2019-07-29 21:52:24 +0300

    feat: add map widget

--- src/pages/author.js
+++ src/pages/author.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import { Link } from 'gatsby';
 import MyMap from '../components/mapWidget';
 
 const AuthorPage = () => {
     return (
         <div>  
-            <MyMap />
+            <div key="/"><Link to='/'>На Главную</Link></div>
+            <MyMap data={[[54.000361, 27.028653], [54.034945, 28.037390], [54.033028, 28.539078], [54.067857, 28.330841]]} />
         </div>
         
     )
