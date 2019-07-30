56a0c2d97ad118876ab7fcb6458ae04823681ab2
GeorgPandeh <georgpandeh@gmail.com>
2019-07-30 22:09:54 +0300

    feat: add base structure of author page and timeLine

--- src/components/mapWidget.js
+++ src/components/mapWidget.js
@@ -2,42 +2,39 @@ import React, { Component } from 'react'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import PropTypes from 'prop-types'
 
-
 class MyMap extends Component {
-    constructor() {  
-        super(); 
+  constructor() {  
+    super(); 
+  }
+
+  render() {
+    // const { coordinates } = this.props;
+    // const position = [this.state.lat, this.state.lng]
+    if (typeof window !== 'undefined') {
+      return (
+        <Map coordinates
+          style={{ height: "480px", width: "500px", margin: '0 auto' }}
+          center={[54, 28]}
+          zoom={5.7}
+          maxZoom={10}
+          doubleClickZoom={true}
+          scrollWheelZoom={true}
+          dragging={true}
+        >
+          <TileLayer 
+            url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" 
+          />
+          {this.props.data.map((element, i) => <Marker key={i} position={element}/> )}
+        </Map>
+      )
     }
-   
-    render() {
-        // const { coordinates } = this.props;
-        // const position = [this.state.lat, this.state.lng]
-        if (typeof window !== 'undefined') {
-        return (
-            <div>
-            <Map coordinates
-                style={{ height: "480px", width: "500px" }}
-                center={[54, 28]}
-                zoom={5.7}
-                maxZoom={10}
-                doubleClickZoom={true}
-                scrollWheelZoom={true}
-                dragging={true}
-                >
-                <TileLayer 
-                    url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" 
-                />
-                {this.props.data.map((element, i) => <Marker key={i} position={element}/> )}
-            </Map>
-            </div>
-        )
-        }
-        return null  
-    } 
+    return null  
+  } 
 }
 
 MyMap.propTypes = {
-    data: PropTypes.array
+  data: PropTypes.array,
 }
 
 export default MyMap;
--- src/pages/author.js
+++ src/pages/author.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Layer from '../components/layer';
+import { Layout, Divider, Row, Col } from 'antd';
 import MyMap from '../components/mapWidget';
+import TimeLine from '../components/timeLine';
 
 const AuthorPage = () => {
-    return (
-        <div>  
-            <div key="/"><Link to='/'>На Главную</Link></div>
+
+  const { Content } = Layout;
+  return (
+    <div>
+      <Layer path='/'>
+        <Layout className="layout">
+          <Content style={{ padding: '0 50px', margin: '0 auto', textAlign: 'center' }}>
+            <img alt="имя" src=""/>
+            <h2>имя</h2>
+            <h3>годы жизни</h3>
+            <Row type="flex" justify="space-around">
+              <Col span={14}>описание описание описание описание описание описание описание описание описание
+                описание описание описание описание описание описание описание описание описание описание
+              </Col>
+            </Row>
+            <Divider/>
             <MyMap data={[[54.000361, 27.028653], [54.034945, 28.037390], [54.033028, 28.539078], [54.067857, 28.330841]]} />
-        </div>
-        
-    )
+            <Divider/>
+            <TimeLine />
+          </Content>
+        </Layout>
+      </Layer>
+    </div>      
+  )
 }
 
 export default AuthorPage;
