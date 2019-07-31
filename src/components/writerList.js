11b420d614d5ab8921da9545fc2e92ebef7de8d3
Yevgeniya.Anossova <Yevgeniya.Anossova@wolterskluwer.com>
2019-08-01 03:45:31 +0600

    feat: add list components, update search feature

---
+++ src/components/writerList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Divider, Col, Row } from 'antd';
+import PropTypes from 'prop-types';
+
+import '../styles/writers.css';
+import WriterPreview from './writerPreview';
+
+const WriterList = ({ writers, colAmount = 4 }) => {
+  const col = [];// подмассив в котором будут массивы по colAmount элемента.
+  let i = 0;
+  while (i < writers.length) {
+    col.push(writers.slice(i, i += colAmount));
+  }
+
+  return (
+    <div className="writers-list">
+      {col.map((items, i) => (
+        <div key={i}>
+          <Row gutter={16}>
+            {items.map((item, j) => (
+              <Col span={6} key={j}>
+                <WriterPreview {...item}/>
+              </Col>
+            ))}
+          </Row>
+          <Divider/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+WriterList.propTypes = {
+  colAmount: PropTypes.number,
+  writers: PropTypes.array.isRequired,
+};
+
+export default WriterList;
