0394ae6d482a2bb88261a70c22cc81e77ebb6f65
Anna Kiniova <anna.kiniova@gmail.com>
2019-08-02 00:46:10 +0300

    style: add button to open video

--- src/components/videoWidget.js
+++ src/components/videoWidget.js
@@ -2,6 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import ModalVideo from 'react-modal-video'
 import '../styles/modal-video.min.css'
+
+import LogoYouTube from '../images/Logo_YouTube.png'
 export default class MyVideo  extends React.Component {
 
   constructor () {
@@ -20,7 +22,10 @@ export default class MyVideo  extends React.Component {
     return (
       <div>
         <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId={this.props.url} onClose={() => this.setState({isOpen: false})} />
-        <button onClick={this.openModal}>Open</button>
+        <a onClick={this.openModal}>
+          <img className='video-button' src={LogoYouTube}>
+          </img>
+        </a>
       </div>
     )
   }
